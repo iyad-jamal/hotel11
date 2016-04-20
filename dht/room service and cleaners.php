2a235bda21f422baa9c2fb5a25c8acc72bdbbda4
iyadjamal <iyadjamal5@gmail.com>
2016-04-20 04:21:16 +0300

    h

--- dht/room service and cleaners.php
+++ dht/room service and cleaners.php
@@ -5,3 +5,19 @@
  * Date: 4/19/2016
  * Time: 12:49 PM
  */
+session_start();
+if(!$_SESSION['login']) {
+
+    ?>
+    <h1 aligo="center" class="text-danger">you must logged in to reach this page</h1>
+    <?php
+    ob_start();
+
+            header("Location:http://localhost:63342/hotel/dht/reg.html");
+            ob_end_flush();
+
+
+    /** YOUR LOGINBOX OUTPUT, ERROR MESSAGES ... **/
+    ob_end_flush();
+}
+?>
